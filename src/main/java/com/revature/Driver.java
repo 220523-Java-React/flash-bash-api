2df7dc882d04468999940af2f2a0399b88ff9809
Matthew Coplan <mcoplanwork@gmail.com>
2022-05-26 15:17:11 -0400

    changed loop iterator

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -12,33 +12,31 @@ public class Driver {
         // objects are runtime entities that contain states (variables) and behaviors (methods)
         // classes are blueprints for objects
         System.out.println("Welcome to FlashBash!!");
-        System.out.println("What would you like to do?");
-        System.out.println("1) Create a Flashcard");
-        System.out.println("2) View all Flashcards");
-        System.out.println("0) Exit\n");
 
-        int flag;
         // it will execute the code once, and then loop if the flag is not 1.
         do {
-            System.out.println("Please enter a menu option.");
+            System.out.println("Please enter a correct menu option.");
+            System.out.println("1) Create a Flashcard");
+            System.out.println("2) View all Flashcards");
+            System.out.println("0) Exit\n");
             String input = scanner.nextLine();
             switch(input) { // only gets evaluated once
                case "1":
                    System.out.println("Create a flashcard");
-                   flag = 1;
+                   System.exit(0);
                    break;
                case "2":
                    System.out.println("View all flashcards");
-                   flag = 1;
+                   System.exit(0);
                    break;
                case "0":
                    System.out.println("Exiting the application");
-                   flag = 1;
+                   System.exit(0);
                    break;
                default:
-                   flag = 0;
+                   break;
            }
-        } while (flag != 1);
+        } while (true);
 
     }
 }
