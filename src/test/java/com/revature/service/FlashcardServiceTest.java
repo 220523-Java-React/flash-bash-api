e13f23ec92e57f8917c13e8f20dcbe4073d578dd
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-06-08 12:34:01 -0500

    finish all test implementations

--- src/test/java/com/revature/service/FlashcardServiceTest.java
+++ src/test/java/com/revature/service/FlashcardServiceTest.java
@@ -23,21 +23,30 @@ public class FlashcardServiceTest {
     }
 
     @Test
-    public void whenGivenFlashObjectCreateNewFlashcardReturnsTrue(){
+    public void whenGivenFlashcardObjectCreateNewFlashcardReturnsTrue(){
         Flashcard flashcard = new Flashcard();
         FlashcardService service = new FlashcardService();
         Mockito.when(mockList.add(flashcard)).thenReturn(true);
-
         boolean result = service.createNewFlashcard(flashcard);
         Assertions.assertTrue(result);
     }
 
+    @Test
+    public void whenGetAllFlashcardsIsCalledReturnsListOfFlashcards(){
+        // this method is also tricky as we can't really ensure the list returns itself
+        // so instead, we once again just assert that this execution doesn't throw an exception
+
+        FlashcardService service = new FlashcardService();
+        Assertions.assertDoesNotThrow(() -> service.getAllFlashcards());
+    }
 
 
     @Test
     public void whenGivenValidIdGetFlashcardByIdReturnsFlashcardWithThatId(){
         Flashcard flashcard = new Flashcard(0, "question", "answer");
 
+        // fake the size of the list when it is called
+        Mockito.when(mockList.size()).thenReturn(1);
         Mockito.when(mockList.get(0)).thenReturn(flashcard);
         FlashcardService service = new FlashcardService(mockList);
         Flashcard result = service.getFlashcardById(0);
