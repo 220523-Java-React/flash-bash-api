ded23682e0e3f0c3de9f42b9693b28806f29f013
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-06-10 14:08:02 -0500

    add DAO interface

---
+++ src/main/java/com/revature/repository/DAO.java
@@ -0,0 +1,32 @@
+package com.revature.repository;
+
+
+/*
+        An interface for the DAO Design Pattern
+            Data Access Objects
+            These objects are dedicated to performing CRUD operations on behalf of a data set.
+
+            Which CRUD operations will apply to every single repository that we create
+ */
+
+import java.util.List;
+
+/*
+        Generics: data type placeholders that allow you to provide the same implementation for different data types
+        while also adhering to type safety
+
+        List<String>
+        List<Flashcard>
+ */
+public interface DAO<T> {
+
+    T create(T t);
+
+    List<T> getAll();
+
+    T getById(int id);
+
+    T update(T t);
+
+    boolean deleteById(int id);
+}
